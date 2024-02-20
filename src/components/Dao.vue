1092e3a3d480f3e4cd98c4a27c8898b450f53604
marlon-bento <marlon_lion@hotmail.com>
2024-02-20 18:33:06 -0300

    melhoras na rota de projetos

--- src/components/Dao.vue
+++ src/components/Dao.vue
@@ -3,18 +3,27 @@
 </template>
 <style scoped>
 .rotating-element {
-            width: 100px;
-            height: 100px;
+    width: 100px;
+    height: 100px;
+    user-drag: none;
+    -webkit-user-drag: none;
+    user-select: none;
+    -moz-user-select: none;
+    -webkit-user-select: none;
+    -ms-user-select: none;
 
-            animation: rotate 3s infinite linear;
-        }
+    animation: rotate 3s infinite linear;
+    
+}
 
-        @keyframes rotate {
-            0% {
-                transform: rotate(0);
-            }
-            100% {
-                transform: rotate(360deg);
-            }
-        }
+
+@keyframes rotate {
+    0% {
+        transform: rotate(0);
+    }
+
+    100% {
+        transform: rotate(360deg);
+    }
+}
 </style>
--- src/components/Minicard.vue
+++ src/components/Minicard.vue
@@ -14,8 +14,17 @@ defineProps(['icone','titulo']);
     padding: 20px;
     background-color: rgba(0, 0, 0, 0.233);
     border: rgb(78, 76, 76) solid 2px;
+    max-width: 180px;
+    overflow: hidden; /* Ocultar conteúdo que ultrapassa as bordas */
 }
 .minicard-icon{
     width: 50px;
+}
+h5{
+    flex: 0; /* Dá ao h5 um espaço flexível para não ultrapassar os limites do contêiner */
+        margin: 1; /* Remove as margens padrão do h5 para garantir um ajuste adequado */
+        white-space: nowrap; /* Impede a quebra de texto para a próxima linha */
+        overflow: hidden; /* Oculta qualquer texto que ultrapasse o contêiner */
+        text-overflow: ellipsis; /* Adiciona reticências (...) ao final do texto que ultrapassa */
 }
 </style>
