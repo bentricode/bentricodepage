d9f5b88ade8fc0a0e49e829b37c89dfe35961bab
Gabriel Hen B <ghbraz17@gmail.com>
2024-02-25 14:34:05 -0300

    Adds more projects to the project route

--- src/views/ProjView.vue
+++ src/views/ProjView.vue
@@ -14,16 +14,20 @@ import PostProj from '@/components/PostProj.vue'
   <main id="conteudo-proj" >
     <div class="container-fluid">
         <PostProj titulo="BentriCode Blog" imagem="/proj1.jpeg"  url="https://github.com/GabrielHenB/bentricode">
-          A web application project written in PHP Laravel and Bootstrap for a fictional development company called Bentricode. We aim to practice front-end and back-end development using this project.
+          Uma aplicação web, escrita em Laravel PHP e Bootstrap para o grupo Bentricode. Foi realizado como prática de desenvolvimento front-end e back-end.
       </PostProj>
-      <PostProj pi="true" titulo="Editor de Fundos de Imagens">
-      
+      <PostProj pi="true" titulo="Editor de Fundos de Imagens" imagem="/proj1.jpeg" url="https://github.com/marlon-bento/api-segmentacao-de-imagens">
+          Desenvolvimento de uma aplicação em Python capaz de remover o fundo de imagens em geral, gerido por uma API em Python Flask.
         <a class="text-light" target="_blank" href="https://bentricode-tirar-fundo-da-imagem.onrender.com/">
-          Link do site de exemplo
+          Acesse o site ao vivo!
         </a>
       </PostProj>
-      <PostProj  titulo="Portal de filmes" imagem="/nazarik.png" url="https://github.com/GabrielHenB/nazarick-movies">
+      <PostProj  titulo="Portal de Filmes com API" imagem="/nazarik.png" url="https://github.com/GabrielHenB/nazarick-movies">
         Portal de filmes realizado com tecnologias emergentes do mercado, como bootstrap, JavaScript, HTML, site totalmente responsivo e dinâmico,
+      </PostProj>
+      <PostProj pi="true" titulo="Campainha Inteligente com App" imagem="/nazarik.png" url="https://github.com/GabrielHenB/campainhaint_iot2">
+        Aplicação de Campainha Inteligente que reconhece o rosto do visitante e o salva em um banco de dados para posterior acesso por meio de aplicativo.
+         O projeto utilizava Vue e Python Flask. A camera foi implementada por meio de um Esp-32-Cam.
       </PostProj>
     </div>
 
